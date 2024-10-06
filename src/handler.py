a8614f2dba6a6c27b73c7fed5f2987212f1b7749
arthurmf <arthurmarcal@gmail.com>
2024-10-06 01:47:09 -0300

    1-define memorySize. 2-Add Lambda Powertools for logger

--- src/handler.py
+++ src/handler.py
@@ -1,17 +1,22 @@
 import json
 import urllib.parse
 
-def hello(event, context):
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+logger = Logger()
+logger._logger.propagate = False
+
+@logger.inject_lambda_context()
+def etl(event: dict, context: LambdaContext) -> str:
     
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-
-    print(f"Bucket: {bucket}")
-    print(f"Key: {key}")
-
+    logger.append_keys(bucket=bucket, key=key)
 
     body = {
         "message": "Go Serverless v4.0! Your function executed successfully!"
     }
 
+    logger.info("Function executed successfully!")
     return {"statusCode": 200, "body": json.dumps(body)}
